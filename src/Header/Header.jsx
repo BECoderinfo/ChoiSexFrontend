d29e05637c1572182403b0313c39e67537a13347
BECoderinfo <becoder.info@gmail.com>
2025-12-08 18:52:26 +0530

    s

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -179,6 +179,21 @@ const Header = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+
+  // Disable body scroll on Drawer Open
+useEffect(() => {
+  if (showDrawer) {
+    document.body.style.overflow = "hidden";   // stop scrolling
+  } else {
+    document.body.style.overflow = "";         // reset scrolling
+  }
+
+  return () => {
+    document.body.style.overflow = "";         // cleanup on unmount
+  };
+}, [showDrawer]);
+
+
   return (
     <>
       {/* HEADER */}
